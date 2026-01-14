1744e641a49578cb11de3a10284c1ecac807e6f3
erfaw <erfawn.h@gmail.com>
2026-01-15 02:14:03 +0330

    in 'Optimal BST\OOP\main.py': 1. add section 'SOME EXAMPLES TO INPUT', 2. add section 'GET ENTRY VALUES FROM USER', 3. in section 'GET ENTRY VALUES FROM USER': get entry values to send as arg to OBST class and built object, 4. in section 'SOME EXAMPLES TO INPUT': comment 2 example to use with this program, 5. in section 'BUILD OBJECT FROM 'OBST' CLASS BASED ON ENTRY VALUES': change args of OBST with these variables,

--- Optimal BST/OOP/main.py
+++ Optimal BST/OOP/main.py
@@ -1,12 +1,27 @@
 from OBST import OBST
-## BUILD OBJECT FROM 'OBST' CLASS
-# obst = OBST(
+import subprocess; subprocess.call('cls', shell=True)
+## SOME EXAMPLES TO INPUT
+#     num_of_nodes = 3,
+#     POSSIBILITY_LIST= [0, 0.7, 0.2, 0.1]
+#   --------
 #     num_of_nodes = 4,
 #     POSSIBILITY_LIST= [0, 0.3, 0.4, 0.2, 0.1]
-# )
+
+## GET ENTRY VALUES FROM USER
+nodes_number = int(input("insert 'num_of_nodes'=\t"))
+possibility_list = []
+print("Please insert possibilities carefully: ")
+for node in range(nodes_number+1):
+    if node == 0: 
+        possibility_list.append(0)
+    else:
+        possibility_list.append(None)
+        possibility_list[node] = float(input(f"\tnode [{node}]=\t"))
+
+## BUILD OBJECT FROM 'OBST' CLASS BASED ON ENTRY VALUES
 obst = OBST(
-    num_of_nodes = 3,
-    POSSIBILITY_LIST= [0, 0.7, 0.2, 0.1]
+    num_of_nodes = nodes_number,
+    POSSIBILITY_LIST= possibility_list
 )
 
 ## CALCULATE BEST OPTIONS
