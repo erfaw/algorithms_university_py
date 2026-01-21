561c27a3bf3118a75a6618d90835d4867672f2df
erfaw <erfawn.h@gmail.com>
2026-01-21 09:20:40 +0330

    in 'Diffie-Hellman Key Exchange\main.py': 1. add cls subprocess, 2. add prints results after each calculation,

--- Diffie-Hellman Key Exchange/main.py
+++ Diffie-Hellman Key Exchange/main.py
@@ -1,4 +1,5 @@
 from client import Client
+import subprocess; subprocess.call('cls', shell=True)
 
 ## make 2 person from Client class
 person_1 = Client()
@@ -18,14 +19,18 @@ person_2.exclusive_number = person_2.generate_exclusive_number()
 
 ## Do calculation for discrete logarithm number and send it to each other
 person_1.discrete_logarithm_cal_to_send = person_1.calculate_discrete_logarithm()
+print(f"this is dicrete logarithm calculated from person_1 to send person_2:\n\t{person_1.discrete_logarithm_cal_to_send}")
 person_2.discrete_logarithm_cal_to_send = person_2.calculate_discrete_logarithm()
+print(f"this is dicrete logarithm calculated from person_2 to send person_1:\n\t{person_2.discrete_logarithm_cal_to_send}")
 
 person_1.recieved_discrete_logarithm_number = person_2.discrete_logarithm_cal_to_send
 person_2.recieved_discrete_logarithm_number = person_1.discrete_logarithm_cal_to_send
 
 ## Do calculation for result key for each person
 person_1.result_key = person_1.calculate_result_key()
+print(f"this is result key of person_1:\n\t{person_1.result_key}")
 person_2.result_key = person_2.calculate_result_key()
+print(f"this is result key of person_2:\n\t{person_2.result_key}")
 
 print()
 
