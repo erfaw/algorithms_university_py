0a3d94eab6a4b6e2d880d2f1ca2d4628d25497c1
erfaw <erfawn.h@gmail.com>
2026-01-21 06:43:52 +0330

    in 'Diffie-Hellman Key Exchange\client.py': 1. import secrets, 2. in 'self.__init__()': add type hint to 'self.private_key:int' because must be int, 3. add method 'self.generate_private_key()' in order to make a 256-bit int number for private_key,

--- Diffie-Hellman Key Exchange/client.py
+++ Diffie-Hellman Key Exchange/client.py
@@ -1,9 +1,18 @@
+import secrets
+
 class Client:
     def __init__(self):
         """Makes a client for the Diffie-Hellman key exchange protocol."""
-        self.private_key = None
+        self.private_key:int = None
         self.confirm_numbers = {
             "prime": None,
             "generator": None,
         }
-        
+    
+    def generate_private_key(self):
+        """
+        Generates a private key which is a random number with any restriction.
+        * Randomness: The private key should be a truly random number to ensure its security. In practice, this means using a cryptographically secure pseudo-random number generator or a hardware-based random number generator.
+        * Large enough: The private key should be large enough to make it computationally infeasible for an attacker to guess or brute-force the value. A common guideline is to use a private key that is at least 256 bits (32 bytes) long.
+        """
+        return secrets.randbits(256)
--- Diffie-Hellman Key Exchange/main.py
+++ Diffie-Hellman Key Exchange/main.py
@@ -3,3 +3,7 @@ from client import Client
 ## make 2 person from Client class
 person_1 = Client()
 person_2 = Client()
+
+## make and assign private key for each person
+person_1.private_key = person_1.generate_private_key()
+person_2.private_key = person_2.generate_private_key()
