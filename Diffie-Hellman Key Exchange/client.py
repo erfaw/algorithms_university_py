20c2322d49b521cd1a847cdf939091b0f87d6514
erfaw <erfawn.h@gmail.com>
2026-01-21 06:48:01 +0330

    in 'Diffie-Hellman Key Exchange\client.py': 1. in 'self.generate_private_key()': change docstring

--- Diffie-Hellman Key Exchange/client.py
+++ Diffie-Hellman Key Exchange/client.py
@@ -9,10 +9,6 @@ class Client:
             "generator": None,
         }
     
-    def generate_private_key(self):
-        """
-        Generates a private key which is a random number with any restriction.
-        * Randomness: The private key should be a truly random number to ensure its security. In practice, this means using a cryptographically secure pseudo-random number generator or a hardware-based random number generator.
-        * Large enough: The private key should be large enough to make it computationally infeasible for an attacker to guess or brute-force the value. A common guideline is to use a private key that is at least 256 bits (32 bytes) long.
-        """
+    def generate_private_key(self) -> int:
+        """Generates a private key with sufficient randomness and size (256-bit)."""
         return secrets.randbits(256)
