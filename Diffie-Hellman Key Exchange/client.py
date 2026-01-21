7c0d41470d7fc3138d0b6174ef6cf069a4f3f0b1
erfaw <erfawn.h@gmail.com>
2026-01-21 09:01:59 +0330

    fully configured, not checked, because with these numbers it took so much time to complete :)

--- Diffie-Hellman Key Exchange/client.py
+++ Diffie-Hellman Key Exchange/client.py
@@ -1,19 +1,38 @@
 import secrets
 import sympy
+import random
 
 class Client:
     def __init__(self):
         """Makes a client for the Diffie-Hellman key exchange protocol."""
-        self.private_key:int = None
-        self.confirm_numbers = {
-            "prime": None,
-            "generator": None,
-        }
-    
-    def generate_private_key(self) -> int:
-        """Generates a private key with sufficient randomness and size (256-bit)."""
-        return secrets.randbits(256)
+        self.p = None
+        self.x = None
+        self.exclusive_number = None
+        self.discrete_logarithm_cal_to_send = None
+        self.recieved_discrete_logarithm_number = None
+
+        self.result_key = None
 
     def generate_big_prime_number(self):
         """Generate a 2048-bit (256-byte) prime number (orginal size)"""
-        return sympy.nextprime(2**11)
+        # Generate a large number of random values
+        random_values = [secrets.randbits(256) for _ in range(1000)]
+
+        # Filter out non-prime numbers
+        prime_numbers = [val for val in random_values if sympy.isprime(val)]
+
+        # Return a random choice of prime numbers
+        return secrets.choice(prime_numbers)
+
+    def generate_x_number(self):
+        return random.randint(2, self.p-2)
+    
+    def generate_exclusive_number(self):
+        return random.randint(2, self.p-2)
+    
+    def calculate_discrete_logarithm(self):
+        return self.x**self.exclusive_number%self.p 
+    
+    def calculate_result_key(self):
+        return (self.recieved_discrete_logarithm_number**self.exclusive_number)%self.p
+        
--- Diffie-Hellman Key Exchange/main.py
+++ Diffie-Hellman Key Exchange/main.py
@@ -4,12 +4,28 @@ from client import Client
 person_1 = Client()
 person_2 = Client()
 
-## make and assign private key for each person
-person_1.private_key = person_1.generate_private_key()
-person_2.private_key = person_2.generate_private_key()
-
 ## agree on big prime number
-person_1.confirm_numbers["prime"] = person_1.generate_big_prime_number()
-person_2.confirm_numbers["prime"] = person_1.confirm_numbers["prime"]
+person_1.p = person_1.generate_big_prime_number()
+person_2.p = person_1.p
 
+## aggre on x number
+person_1.x = person_1.generate_x_number()
+person_2.x = person_1.x
+
+## pick exclusive numbers for each person
+person_1.exclusive_number = person_1.generate_exclusive_number()
+person_2.exclusive_number = person_2.generate_exclusive_number()
+
+## Do calculation for discrete logarithm number and send it to each other
+person_1.discrete_logarithm_cal_to_send = person_1.calculate_discrete_logarithm()
+person_2.discrete_logarithm_cal_to_send = person_2.calculate_discrete_logarithm()
+
+person_1.recieved_discrete_logarithm_number = person_2.discrete_logarithm_cal_to_send
+person_2.recieved_discrete_logarithm_number = person_1.discrete_logarithm_cal_to_send
+
+## Do calculation for result key for each person
+person_1.result_key = person_1.calculate_result_key()
+person_2.result_key = person_2.calculate_result_key()
+
+print()
 
