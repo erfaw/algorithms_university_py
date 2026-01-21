202de2c282df858479e02241d2552109f2de1dc5
erfaw <erfawn.h@gmail.com>
2026-01-21 09:07:04 +0330

    in 'Diffie-Hellman Key Exchange\main.py': 1. fill test values to check functionality of algorithm, RUN-CHECK: good, we get right result

--- Diffie-Hellman Key Exchange/main.py
+++ Diffie-Hellman Key Exchange/main.py
@@ -5,16 +5,16 @@ person_1 = Client()
 person_2 = Client()
 
 ## agree on big prime number
-person_1.p = person_1.generate_big_prime_number()
+person_1.p = 11
 person_2.p = person_1.p
 
 ## aggre on x number
-person_1.x = person_1.generate_x_number()
+person_1.x = 7
 person_2.x = person_1.x
 
 ## pick exclusive numbers for each person
-person_1.exclusive_number = person_1.generate_exclusive_number()
-person_2.exclusive_number = person_2.generate_exclusive_number()
+person_1.exclusive_number = 5
+person_2.exclusive_number = 8
 
 ## Do calculation for discrete logarithm number and send it to each other
 person_1.discrete_logarithm_cal_to_send = person_1.calculate_discrete_logarithm()
@@ -27,5 +27,6 @@ person_2.recieved_discrete_logarithm_number = person_1.discrete_logarithm_cal_to
 person_1.result_key = person_1.calculate_result_key()
 person_2.result_key = person_2.calculate_result_key()
 
-print()
+print(f"this is result key for person_1:\n\t{person_1.result_key}")
+print(f"this is result key for person_2:\n\t{person_2.result_key}")
 
